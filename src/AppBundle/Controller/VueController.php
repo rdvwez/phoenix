6082f98b353dc2c67ff5995b6fdb741e803ae5ba
vwez <desirv63@gmail.com>
2019-06-05 22:04:23 +0200

    envoi de mail achevé

--- src/AppBundle/Controller/VueController.php
+++ src/AppBundle/Controller/VueController.php
@@ -72,7 +72,7 @@ class VueController extends Controller
 	* @Route("/Me_Contacter",name="Me_contacter")
 	*@Route("/enregistrer_messages", name="enregistrer_message")
 	*/
-	public function Contacter(Request $request)
+	public function Contacter( Request $request,\Swift_Mailer $mailer)
 	{
 		$entityManager = $this->getDoctrine()->getManager();
 		$messages = new Messages();
@@ -82,28 +82,46 @@ class VueController extends Controller
 			->add('contenu',TextareaType::class)
 			// ->add('createdAt',DateTimeType::class)
 			->getForm();
-			$dt = new \DateTime();
-			
-			
-			$form->handleRequest($request);
+		$dt = new \DateTime();
+		$code = null;
+					
+		$form->handleRequest($request);
+		
+		  
+		
 
+		if ($form->isSubmitted() && $form->isValid()) 
+		{	
+			$code = 1;
+			// dump($messages->getContenu());
 			
-
-			if ($form->isSubmitted() && $form->isValid()) {
-				
-				
+			
 				$messages->setCreatedAt($dt);
 				// dump($messages);
-        $messages = $form->getData();
-				$entityManager->persist($messages);
-				$entityManager->flush();
+        // $messages = $form->getData();
+				// $entityManager->persist($messages);
+				// $entityManager->flush();
+				
+				$messageToSend = (new \Swift_Message('Accusé de réception de votre message'))
+					->setFrom('vwezrd@gmail.com')
+					->setTo($messages->getEmail())
+					->setBody('
+					Bonjour, votre message a été bien enregistré, je vous répondrai dans les plus bref delai.
+					Coordialement.
+					');
+					$mailer->send($messageToSend);
+					$messages = new Messages();
 
-        return $this->redirectToRoute('Me_contacter',['code'=>'success']);
+				// $request = Request::createFromGlobals();
+				// $request->query->get('nom','success');
+				// return $this->redirectToRoute('Me_contacter', ['code' => 	$code]);
+				
     }
 			
 		return $this->render('pages/contacts.html.twig', [
-            'formMessages' => $form->createView(),
-        ]);
+						'formMessages' => $form->createView(),
+						'code' => 	$code
+    ]);
 
 	}
 
@@ -113,7 +131,7 @@ class VueController extends Controller
 	public function InformationsPersonnelles()
 	{
 		return $this->render('pages/Affichage.html.twig' );
-    }
+  }
     
     
 }
